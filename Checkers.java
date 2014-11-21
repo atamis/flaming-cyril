6ce4004b2c5b5f816e677691912aa30042f5fc64
Desyderata <ncare@oberlin.edu>
2014-11-20 21:45:50 -0500

    updated checkers with method

--- Checkers.java
+++ Checkers.java
@@ -1,2 +1,21 @@
 package grouplab;
 
+/**
+ * Checkers game implementation
+ * 
+ * @author Nick Care
+ * @author Andrew Amis
+ * @author Sam Goree
+ * @author Gabe Appleby
+ * 
+ */
+ 
+public class Checkers {
+  
+  // plays a game of checkers
+  public static void play() {
+    
+  }
+  
+}
+
