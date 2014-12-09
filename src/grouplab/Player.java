38cf07bb28b0caad30816c2d600ece8c257b4891
Nick Care <NickCare@ip-90-12.wireless.oberlin.edu>
2014-12-09 16:11:59 -0500

    added comments to player

--- src/grouplab/Player.java
+++ src/grouplab/Player.java
@@ -1,8 +1,15 @@
 package grouplab;
 
-
 import java.util.LinkedList;
 
+/**
+* Interface that should be implemented by all players
+*
+* @author Nick Care (2014)
+* @author Andrew Amis (2014)
+* @author Sam Goree (2014)
+* @author Gabe Appleby (2014)
+*/
 public interface Player {
 	// asks the player for a move
 	public Move queryMove(Board b, LinkedList<Move> moves);
@@ -12,5 +19,5 @@ public interface Player {
 
 	// is the player human
 	public boolean isHuman();
-	
+
 }
